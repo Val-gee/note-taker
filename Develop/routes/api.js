71be294d413179f37a0bc08732f9cd53ae401178
val.gee <vguevara0806@gmail.com>
2023-01-04 16:38:16 -0500

    deleted unnecessary files and comments; added screenshot and link to deployed websit in README file

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -22,12 +22,12 @@ api.get('/:id', (req, res) => {
         });
 });
 
-api.delete('/id', (req, res) => {
+api.delete('/api/notes/:id', (req, res) => {
     // let notes = JSON.parse(fs.readFileSync(path.join(__dirname, '../db/db.json')));
     // let newNotes = notes.filter(note => note.id !== req.params.id);
     // fs.writeFileSync(path.join(__dirname, '../db/db.json'), JSON.stringify(newNotes));
     // res.json(newNotes);
-    const currentNoteId = req.params.notes_id;
+    const currentNoteId = req.params.id;
     console.log(currentNoteId);
 
     readFromFile('../db/notes.json')
@@ -35,7 +35,7 @@ api.delete('/id', (req, res) => {
         .then((data) => JSON.parse(data))
         .then((json) => {
             //make a new array of all the notes except the one with the ID provided in the url
-            const result = json.filter((note) => note.notes_id !== currentNoteId);
+            const result = json.filter((note) => note.id !== currentNoteId);
             console.log(result);
             
             //save that new array to the file system
